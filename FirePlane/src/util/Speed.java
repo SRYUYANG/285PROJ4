ce71a4dcac2cb62d3e218999fad33cd57ace2c22
renl <renl@umich.edu>
2014-11-15 17:50:08 -0500

    make it smooth
    now the movements have become smooth

---
+++ FirePlane/src/util/Speed.java
@@ -0,0 +1,31 @@
+package util;
+
+public class Speed
+{
+  private double xSpeed, ySpeed;
+  public Speed(double _xSpeed, double _ySpeed)
+  {
+    xSpeed = _xSpeed;
+    ySpeed = _ySpeed;
+  }
+  
+  public double getXSpeed()
+  {
+    return xSpeed;
+  }
+  
+  public double getYSpeed()
+  {
+    return ySpeed;
+  }
+  
+  public void setXSpeed(double inX)
+  {
+    xSpeed = inX;
+  }
+  
+  public void setYSpeed(double inY)
+  {
+    ySpeed = inY;
+  }
+}
