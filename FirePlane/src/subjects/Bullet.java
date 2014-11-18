d84de9e6196f0d8b232d3335e833d207cb58e69f
cyuyang@umich.edu <cyuyang@umich.edu>
2014-11-17 21:31:11 -0500

    Small Modification

---
+++ FirePlane/src/subjects/Bullet.java
@@ -0,0 +1,13 @@
+package subjects;
+
+import java.awt.Graphics;
+
+public class Bullet extends Stuff {
+
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ FirePlane/src/subjects/SmallBullet.java
@@ -0,0 +1,32 @@
+package subjects;
+
+import java.awt.image.BufferedImage;
+
+import util.Location;
+import util.Speed;
+
+public class SmallBullet extends Bullet
+{
+
+  @Override
+  public Location getLocation()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public Speed getSpeed()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public BufferedImage getImage()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
