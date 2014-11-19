4385c8ea1b4a99e7163821756c0f94d763f3d8e2
cyuyang@umich.edu <cyuyang@umich.edu>
2014-11-19 16:16:39 -0500

    Add explosion and Bullet

---
+++ FirePlane/src/subjects/EnemyPlane.java
@@ -0,0 +1,24 @@
+package subjects;
+
+import java.awt.image.BufferedImage;
+
+import util.Location;
+
+public class EnemyPlane extends Plane
+{
+  
+
+  public EnemyPlane(Location loc)
+  {
+    super(loc);
+    // TODO Auto-generated constructor stub
+  }
+
+  @Override
+  public BufferedImage getImage()
+  {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
